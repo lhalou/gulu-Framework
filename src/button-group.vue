bd88df8485ab9b6b90585115c0a7b63ae4c046bf
lliqi <614124284@qq.com>
2020-03-11 21:11:48 +0800

    安装chai

--- src/button-group.vue
+++ src/button-group.vue
@@ -7,11 +7,12 @@
 
 <script>
     export default {
+        //防止用户随意嵌套标签
         mouted(){
             for (let node of this.$el.children){
                 let name = node.nodeName.toLowerCase()
                 if( name !== "button"){
-                    console.log(`g-button-group的子元素应该全是g-button,但是你写的是${name}`)
+                    console.warn(`g-button-group的子元素应该全是g-button,但是你写的是${name}`)
                 }
             }
         }
