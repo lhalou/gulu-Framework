890064176986264e06929789dfbfed6fee12fc10
lliqi <614124284@qq.com>
2020-03-11 21:41:05 +0800

    使用chai.expect进行单元测试button

--- src/app.js
+++ src/app.js
@@ -13,3 +13,22 @@ new Vue({
         loading3: false
     }
 })
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+    //动态生成一个按钮
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'settings'
+        }
+    })
+    button.$mount()//挂载到内存中，没有报错，则测试正确
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement)
+    //如果主管判断和实际是一样的，则测试通过
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+}
